9a76e68d45cf879994958761b27fe96803db007e
AltPerson <nothinghardforyou@gmail.com>
2023-08-31 18:27:39 +0300

    Add focus on editArea

--- src/components/Content/Workspace/Editarea.jsx
+++ src/components/Content/Workspace/Editarea.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useContext, useRef } from 'react';
 import { updateNote } from '../../../data/db';
 import { NotesContext } from '../../../data/Context';
 
@@ -6,15 +6,28 @@ function Editarea({ item, forEdit }) {
   const { area } = useContext(NotesContext);
   const [areaText, setAreaText] = area;
   const [value, setValue] = useState(forEdit ? item.text : '');
+  const textAreaRef = useRef(null);
+
+  useEffect(() => {
+    textAreaRef && textAreaRef.current.focus();
+  }, []);
 
   useEffect(() => {
     forEdit && updateNote(value, item.id, Date.now());
   }, [forEdit, item?.id, value]);
+
   return (
     <div className="workspace-editarea">
       <textarea
+        ref={textAreaRef}
         className="workspace-editarea__text"
         value={forEdit ? value : areaText}
+        onFocus={(e) =>
+          e.currentTarget.setSelectionRange(
+            e.currentTarget.value.length,
+            e.currentTarget.value.length,
+          )
+        }
         onChange={(e) => {
           forEdit ? setValue(e.target.value) : setAreaText(e.target.value);
         }}
